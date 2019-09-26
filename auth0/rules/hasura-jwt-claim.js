7d2eb0ab849d393120a8e619e34d77343cf8991d
Vincent Grafe <vincent.grafe@gmail.com>
2019-09-25 20:37:55 -0400

    updates claim rule to return db id

--- auth0/rules/hasura-jwt-claim.js
+++ auth0/rules/hasura-jwt-claim.js
@@ -1,10 +1,33 @@
 function hasuraJwtClaim(user, context, callback) {
-  const namespace = 'https://hasura.io/jwt/claims';
-  context.idToken[namespace] = {
-    'x-hasura-default-role': 'user',
-    // do some custom logic to decide allowed roles
-    'x-hasura-allowed-roles': ['user'],
-    'x-hasura-user-id': user.user_id
+  const QUERY_BODY = {
+    // could get roles here
+    query: `{
+              user(where: {auth0_user_id: {_eq: "${user.user_id}"}}) {
+                  id
+                }
+            }`
   };
-  callback(null, user, context);
+
+  request.post(
+    {
+      headers: {
+        'content-type': 'application/json',
+        'x-hasura-admin-secret': configuration.ADMIN_SECRET
+      },
+      url: configuration.HASURA_GRAPHQL_URL,
+      body: JSON.stringify(QUERY_BODY)
+    },
+    function(error, response, body) {
+      if (error) console.log('error', error);
+      else {
+        console.log('body', body);
+        context.idToken['https://hasura.io/jwt/claims'] = {
+          'x-hasura-default-role': 'user',
+          'x-hasura-allowed-roles': ['user'], // can add custom logic to decide allowed roles
+          'x-hasura-user-id': JSON.parse(body).data.user[0].id
+        };
+        callback(null, user, context);
+      }
+    }
+  );
 }
--- auth0/rules/upsert-user.js
+++ auth0/rules/upsert-user.js
@@ -2,7 +2,7 @@ function upsertUser(user, context, callback) {
   const userId = user.user_id;
   const nickname = user.nickname;
 
-  const body2 = {
+  const QUERY_BODY = {
     query:
       'mutation ($userId: bpchar!, $nickname: String) {  insert_user(objects: {name: $nickname, auth0_user_id: $userId}, on_conflict: {constraint: user_auth0_user_id_key, update_columns: name}) {    affected_rows  }}',
     variables: { userId: userId, nickname: nickname }
@@ -15,7 +15,7 @@ function upsertUser(user, context, callback) {
         'x-hasura-admin-secret': configuration.ADMIN_SECRET
       },
       url: configuration.HASURA_GRAPHQL_URL,
-      body: JSON.stringify(body2)
+      body: JSON.stringify(QUERY_BODY)
     },
     function(error, response, body) {
       if (error) console.log('error', error);
