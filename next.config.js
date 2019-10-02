54f132eb513b73fb1273624a00bcca7886e79beb
Vincent Grafe <vincent.grafe@gmail.com>
2019-10-02 13:13:12 -0400

    more explicit syntax, adds scope for token refresh

--- next.config.js
+++ next.config.js
@@ -2,9 +2,17 @@ const { parsed: localEnv } = require('dotenv').config();
 const webpack = require('webpack');
 
 module.exports = {
-  webpack(config) {
-    config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
-
-    return config;
+  env: {
+    AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
+    AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+    AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
+    AUTH0_SCOPE: 'openid profile offline_access',
+    REDIRECT_URI:
+      process.env.REDIRECT_URI || 'http://localhost:3000/api/callback',
+    POST_LOGOUT_REDIRECT_URI:
+      process.env.POST_LOGOUT_REDIRECT_URI || 'http://localhost:3000/',
+    SESSION_COOKIE_SECRET: process.env.SESSION_COOKIE_SECRET,
+    HASURA_GRAPHQL_URL: process.env.HASURA_GRAPHQL_URL,
+    SESSION_COOKIE_LIFETIME: 7200 // 2 hours
   }
 };
