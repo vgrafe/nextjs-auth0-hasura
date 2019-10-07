2924c98709335de30fbdc4a7eebab6886b7243ca
Vincent Grafe <vincent.grafe@gmail.com>
2019-10-07 16:12:49 -0400

    improves landing page info

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,23 @@
-import React from 'react'
+import React from 'react';
 
-import Layout from '../components/layout'
-import { useFetchUser } from '../lib/user'
+import Layout from '../components/layout';
+import { useFetchUser } from '../lib/user';
 
-function Home () {
-  const { user, loading } = useFetchUser()
+function Home() {
+  const { user, loading } = useFetchUser();
 
   return (
     <Layout user={user} loading={loading}>
-      <h1>Next.js and Auth0 Example</h1>
-
-      {loading && <p>Loading login info...</p>}
-
-      {!loading && !user && (
-        <>
-          <p>
-            To test the login click in <i>Login</i>
-          </p>
-          <p>
-            Once you have logged in you should be able to click in{' '}
-            <i>Profile</i> and <i>Logout</i>
-          </p>
-        </>
-      )}
-
-      {user && (
-        <>
-          <h4>Rendered user info on the client</h4>
-          <img src={user.picture} alt='user picture' />
-          <p>nickname: {user.nickname}</p>
-          <p>name: {user.name}</p>
-        </>
-      )}
+      <h1>Next.js + Auth0 + Hasura</h1>
+      <p>
+        To test the login click in <i>Login</i>
+      </p>
+      <p>
+        Once you have logged in you should be able to see the logout link and
+        the graphql/profile demo pages
+      </p>
     </Layout>
-  )
+  );
 }
 
-export default Home
+export default Home;
